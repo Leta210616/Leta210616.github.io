6dec138aca0025ffa892617a222f67a7d6635a79
Leta210616 <74385152+Leta210616@users.noreply.github.com>
2023-08-31 11:53:01 -0500

    Update conexionDB.php

--- conexionDB.php
+++ conexionDB.php
@@ -1,8 +1,8 @@
 <?php
 # coneccion a la base de datos
-$host="pruebatecnica.mysql.database.azure.com";
-$user="luis";
-$pass="enrique123456789*";
+$host="localhost";
+$user="root";
+$pass="";
 $db_name="tareas";
 $link=mysqli_connect($host, $user, $pass, $db_name);
 ?>
