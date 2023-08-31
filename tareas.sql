1a9c4eb485c261e1993ab46a7c0a341c8777d567
Leta210616 <74385152+Leta210616@users.noreply.github.com>
2023-08-30 22:59:17 -0500

    commit sql

--- tareas.sql
+++ tareas.sql
@@ -1,1 +1,1 @@
-CREATE TABLE `taks` (   `id` int(11) NOT NULL AUTO_INCREMENT,   `tarea` varchar(200) NOT NULL,   `estado` int(11) NOT NULL,   PRIMARY KEY (`id`) ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE `taks` (   `id` int(11) NOT NULL AUTO_INCREMENT,   `tarea` varchar(200) NOT NULL,   `estado` int(11) NOT NULL,  . PRIMARY KEY (`id`) ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
